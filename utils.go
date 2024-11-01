b7018f9bd61000a247fb956e2966b6c86793d19d
clomollo <oyoocliff471@gmail.com>
2024-11-01 11:35:11 +0300

    Add utils.go that has utility functions used within the program

---
+++ utils.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net"
+)
+
+func broadcast(message string){
+	for _,client:=range clients{
+		client.conn.Write([]byte(message+"\n"))
+	}
+}
+
+func sendPreviousMessages(conn net.Conn){
+	for _,msg:=range messageLog{
+		conn.Write([]byte(msg+"\n"))
+	}
+}
+
+func welcomeMessage() string {
+	return `Welcome to TCP-Chat!
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    .       |' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     -'       --'
+`
+}
+
